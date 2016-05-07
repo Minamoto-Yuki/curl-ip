97bfdbdd011f659f2a0c317062d8058828b30882
Yuki Minamoto <yuki.minamoto.10@gmail.com>
2016-05-07 15:15:16 +0900

    Edit index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?PHP
-echo $_SERVER["HTTP_USER_AGENT"];
-if(strpos($_SERVER["HTTP_USER_AGENT"],"Curl")!==false){
+//echo $_SERVER["HTTP_USER_AGENT"];
+if(strpos($_SERVER["HTTP_USER_AGENT"],"curl")!==false){
 echo $_SERVER["REMOTE_ADDR"];
 }
 ?>
