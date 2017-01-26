934e618110f86000c51d684a35968702a8d28f0c
MinamotoYuki <yuki.minamoto.10@gmail.com>
2017-01-26 19:43:54 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?PHP
 header("Content-Type: text/plain");
 if(strpos($_SERVER["HTTP_USER_AGENT"],"curl")!==false){
+  echo "## Your IP is ";
   echo $_SERVER["HTTP_X_FORWARDED_FOR"];
+  echo " ##\n";
+  echo "-- powerted by https://curl-ip.herokuapp.com/ :)"
 }else{
- echo "curl http://curl-ip.herokuapp.com/";
+ echo "curl https://curl-ip.herokuapp.com/";
 }
 ?>
