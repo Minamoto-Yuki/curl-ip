0e030e2880373d995175211b5df84cbede2dae75
MinamotoYuki <yuki.minamoto.10@gmail.com>
2017-01-26 19:46:39 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?PHP
 header("Content-Type: text/plain");
 if(strpos($_SERVER["HTTP_USER_AGENT"],"curl")!==false){
-  echo "## Your IP is ";
+  echo "-------------------------------------\n";
+  echo "--> Your IP is ";
   echo $_SERVER["HTTP_X_FORWARDED_FOR"];
   echo " ##\n";
-  echo "-- powerted by https://curl-ip.herokuapp.com/ :)";
+  echo "--> powerted by https://curl-ip.herokuapp.com/ :) <--";
+  echo "-------------------------------------";
 }else{
  echo "curl https://curl-ip.herokuapp.com/";
 }
