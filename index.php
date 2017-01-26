27dba4d14ef553d4761c6a69e422c404daaa154f
MinamotoYuki <yuki.minamoto.10@gmail.com>
2017-01-26 19:40:32 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?PHP
 header("Content-Type: text/plain");
-//echo $_SERVER["HTTP_USER_AGENT"];
 if(strpos($_SERVER["HTTP_USER_AGENT"],"curl")!==false){
-echo $_SERVER["HTTP_X_FORWARDED_FOR"];
+  echo $_SERVER["HTTP_X_FORWARDED_FOR"];
+}else{
+ echo "curl http://curl-ip.herokuapp.com/";
 }
 ?>
